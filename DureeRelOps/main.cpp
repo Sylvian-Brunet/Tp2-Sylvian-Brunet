34c9a52f53ed196050f9c8f788f5e9405a9381d4
Sylvian Brunet <sylvian.brunet@etu.univ-amu.fr>
2018-03-29 12:15:41 +0200

    ajout Exo 2 et 3

--- DureeRelOps/main.cpp
+++ DureeRelOps/main.cpp
@@ -1,9 +1,117 @@
 #include <iostream>
+#include <iomanip>         // boolalpha
+#include <utility>
+#include <vector>
 
 using namespace std;
+using namespace rel_ops;
 
-int main(int argc, char *argv[])
+#include "duree.h"
+
+using namespace nsUtil;
+
+#define classdef typedef
+
+namespace
 {
-    cout << "Hello World!" << endl;
+    void testDuree (void)
+    {
+        Duree d1 (3661);
+        Duree d2 (2661);
+        Duree d3 (3661);
+
+        cout << boolalpha;
+        /*         */
+        d1.display ();
+        cout << " <  ";
+        d2.display ();
+        cout << " : " << (d1 <  d2) << '\n';
+
+        d2.display ();
+        cout << " <  ";
+        d1.display ();
+        cout << " : " << (d2 <  d1) << "\n\n";
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " >  ";
+        d2.display ();
+        cout << " : " << (d1 >  d2) << '\n';
+
+        d2.display ();
+        cout << " >  ";
+        d1.display ();
+        cout << " : " << (d2 >  d1) << "\n\n";
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " <= ";
+        d2.display ();
+        cout << " : " << (d1 <= d2) << '\n';
+
+        d2.display ();
+        cout << " <= ";
+        d1.display ();
+        cout << " : " << (d2 <= d1) << "\n\n";
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " >= ";
+        d2.display ();
+        cout << " : " << (d1 >= d2) << '\n';
+
+        d2.display ();
+        cout << " >= ";
+        d1.display ();
+        cout << " : " << (d2 >= d1) << "\n\n";
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " == ";
+        d2.display ();
+        cout << " : " << (d1 == d2) << '\n';
+
+        d2.display ();
+        cout << " == ";
+        d1.display ();
+        cout << " : " << (d2 == d1) << "\n\n";
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " == ";
+        d3.display ();
+        cout << " : " << (d1 == d3) << '\n';
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " != ";
+        d2.display ();
+        cout << " : " << (d1 != d2) << '\n';
+        /*         */
+
+        /*         */
+        d1.display ();
+        cout << " != ";
+        d3.display ();
+        cout << " : " << (d1 != d3) << '\n';
+        /*         */
+
+        cout << noboolalpha;
+
+    } // testDuree()
+
+} // namespace anonyme
+
+int main (void)
+{
+    testDuree ();
+
     return 0;
-}
+
+} // main()
